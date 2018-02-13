284590011760fc9485340b9b2be7dea117c36263
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-02-13 15:40:12 -0600

    hero

---
+++ hero.py
@@ -0,0 +1,17 @@
+class Hero:
+    def __init__(self, health, power)
+        self.health = health
+        self.power = power
+class Goblin:
+    def __init__(self, health, power)
+        self.health = health
+        self.power = power
+    def attack(self, hero):
+    hero.attack(goblin)
+    def attack(self, goblin):
+    goblin.attack(hero)
+
+
+hero = Hero(10,5)
+goblin = Goblin(6,2)
+
