07a89c7125ddd57fffec3de3d25f51d5a1333c7d
Nhatchet <NhaNguyen@GTC3-MBP.localdomain>
2018-02-14 11:31:42 -0600

    RPG

--- hero.py
+++ hero.py
@@ -1,17 +1,56 @@
-class Hero:
+class Character:
+     def alive(self):
+       if self.health < 0:
+           return False 
+
+
+
+class Hero(Character):
     def __init__(self, health, power)
         self.health = health
         self.power = power
-class Goblin:
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print("You do {} damage to the goblin.".format(self.power))
+            if enemy.health <= 0:
+                print("The goblin is dead.") 
+    def print_status(self):
+        print("You have {} health and {} power.".format(self.health, self.power))
+
+
+class Goblin(Character):
     def __init__(self, health, power)
         self.health = health
         self.power = power
-    def attack(self, hero):
-    hero.attack(goblin)
-    def attack(self, goblin):
-    goblin.attack(hero)
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print("The goblin does {} damage to you.".format(self.power))
+            if enemy.health <= 0:
+                print("You are dead.")
+
+    def print_status(self):
+        print("The goblin has {} health and {} power.".format(self.health, self.poer))
+
+
+       
 
 
 hero = Hero(10,5)
 goblin = Goblin(6,2)
+while goblin.alive() and hero.alive():
+    print()
+    print("What do you want to do?")
+    print("1. fight goblin")
+    print("2. do nothing")
+    print("3. flee")
+    print("> ", end=' ')
+    raw_input = input()
+    if raw_input == "1":
+        hero.attack(goblin)
+        if goblin.health > 0:
+            goblin.attack(hero)
+
+
+
+
 
