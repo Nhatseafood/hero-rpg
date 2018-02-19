8bfa6c57d71a3566efd7b1a25251d4ae198b1d08
Nhatchet <NhaNguyen@GTC3-MacBook-Pro.local>
2018-02-19 08:59:38 -0600

    rpg

--- hero.py
+++ hero.py
@@ -14,14 +14,20 @@ class Hero(Character):
         self.power = power
         self.double_attack = 0.2
     def attack(self, enemy):
-        if random.random() < self.double_attack:
-            enemy.health -= self.power * 2
+        if (random.random() < self.double_attack):
+            enemy.rem_hp(self.power * 2)
+            print("Critical HIT!")
+            print("You do {} damage to the goblin.".format(self.power * 2))
+            
         else:
-            enemy.health -= self.power
+            enemy.rem_hp(self.power)
+            print("You do {} damage to the goblin.".format(self.power))
 
-        print("You do {} damage to the goblin.".format(self.power))
         if enemy.health <= 0:
                 print("The goblin is dead.") 
+    def rem_hp(self, power):
+        self.health -= power
+    
     def print_status(self):
         print("You have {} health and {} power.".format(self.health, self.power))
 
@@ -31,31 +37,62 @@ class Goblin(Character):
         self.health = health
         self.power = power
     def attack(self, enemy):
-        enemy.health -= self.power
+        # enemy.health -= self.power
+        enemy.rem_hp(self.power)
         print("The goblin does {} damage to you.".format(self.power))
         if enemy.health <= 0:
                 print("You are dead.")
-
+    def rem_hp(self, power):
+        self.health -= power
     def print_status(self):
-        print("The goblin has {} health and {} power.".format(self.health, self.poer))
+        print("The goblin has {} health and {} power.".format(self.health, self.power))
 
 class Medic(Character):
     def __init__(self, health, power):
         self.health = health
         self.power = power
-        self.heal = heal
+        self.heal = 0.2
     def attack(self, enemy):
-        enemy.health -= self.power
+        enemy.rem_hp(self.power)
+
+    def heal(self): 
+        if random.random() < self.heal:
+            self.health += 2
+            enemy.health -= self.power * 2
+        else:
+            enemy.health -= self.power
+
+
         print("The goblin does {} damage to you.".format(self.power))
         if enemy.health <= 0:
                 print("You are dead.")
-
+    
+    def rem_hp(self, power):
+        self.health -= power
+    
     def print_status(self):
-        print("The goblin has {} health and {} power.".format(self.health, self.poer))
+        print("The medic has {} health and {} power.".format(self.health, self.power))
+
+class Shadow(Character):
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+        self.miss = 0.1
+    def attack(self, enemy):
+        enemy.rem_hp(self.power)
+    
+    def rem_hp(self, power):
+        self.health -= power
+    def print_status(self):
+        print("The Shadow has {} health and {} power.".format(self.health, self.power))
+
+
 
 
 hero = Hero(10,5)
 goblin = Goblin(6,2)
+medic = Medic(10,5)
+shadow = Shadow(1,5)
 while goblin.alive() and hero.alive():
     print()
     print("What do you want to do?")
@@ -76,7 +113,13 @@ while goblin.alive() and hero.alive():
     else:
         print("Invalid input {}".format(raw_input))
 
+    goblin.attack(hero)
 
-
+        if goblin.attack(hero) > 0:
+            # Goblin attacks hero
+            hero.health -= goblin.power
+            print("The goblin does {} damage to you.".format(goblin_power))
+            if hero.health <= 0:
+                print("You are dead.")
 
 
